ff96ba491698d4d9d56d870956f7d7d24073ab67
Colin Simon <s81601@htw-dresden.de>
2022-09-15 19:31:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Explanation on how to run on Taurus
+
+First you need to create a symlink from the scrath folder storing the code to your personal home folder.
+
+The scripts assume that your scripts reside in /home/<USERNAME>/scratch
+
+`ln -s /scratch/ws/0/cosi765e-python_virtual_environment scratch`
---
+++ run_experiment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+                           
+#SBATCH --no-requeue
+#SBATCH --partition=alpha
+#SBATCH --nodes=1                   
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8000
+#SBATCH --mincpus=1
+#SBATCH --time=24:00:00                             
+#SBATCH --job-name=vgg_mcdropout_cifar10
+#SBATCH --mail-type=end
+#SBATCH --mail-user=colin.simon@mailbox.tu-dresden.de
+#SBATCH --output=output-%j.out
+#SBATCH --error=error-%j.out
+
+module --force purge                          				
+module load modenv/hiera CUDA/11.3.1 GCC/11.2.0 Python/3.9.6
+
+source lib.sh
+
+create_or_reuse_environment
+
+cd /home/"$USER"/scratch/BAAL_EXPERIMENTS/
+
+python vgg_mcdropout_cifar10.py
