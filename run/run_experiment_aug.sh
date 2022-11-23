bcd4309f94cf6679a3f394c792afd8302485bb70
Colin Simon <s81601@htw-dresden.de>
2022-11-23 08:23:52 +0100

    prepare new experiment

--- run/run_experiment_aug.sh
+++ run/run_experiment_aug.sh
@@ -4,10 +4,10 @@
 #SBATCH --partition=alpha
 #SBATCH --nodes=1                   
 #SBATCH --gres=gpu:1
-#SBATCH --mem=16000
+#SBATCH --mem=8000
 #SBATCH --mincpus=1
-#SBATCH --time=1:00:00                             
-#SBATCH --job-name=cifarnet_mcdropout_1_augmented_100_epochs
+#SBATCH --time=48:00:00                             
+#SBATCH --job-name=augmented_2_epoch_75
 #SBATCH --mail-type=end
 #SBATCH --mail-user=colin.simon@mailbox.tu-dresden.de
 #SBATCH --output=output-%j.out
@@ -24,4 +24,4 @@ create_or_reuse_environment
 
 cd /home/"$USER"/scratch/BaaL_EXPERIMENTS/
 
-python ../cifarnet_mcdropout_augmented_cifar10.py --augment 2 --epoch 10
+python ../cifarnet_mcdropout_augmented_cifar10.py --augment 2 --epoch 75
